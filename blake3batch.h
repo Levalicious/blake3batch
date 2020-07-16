a5a9d17711481064afdbc92622d39ee7c4274ccd
Lev Knoblock <levknoblock@gmail.com>
2020-07-16 04:05:49 -0400

    Update blake3batch.h
    Fixed header guards

--- blake3batch.h
+++ blake3batch.h
@@ -1,5 +1,5 @@
-#ifndef STARKLIB_BLAKE3BATCH_H
-#define STARKLIB_BLAKE3BATCH_H
+#ifndef BLAKE3BATCH_H
+#define BLAKE3BATCH_H
 
 unsigned char* blake3batch(unsigned char* input, const unsigned int n, const unsigned int size);
 
